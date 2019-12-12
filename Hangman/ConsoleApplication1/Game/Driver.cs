7a5640c70f7688bef247e402ae76f5ffa9a4aa14
Marissa Kuehn <kuehnmarissa@gmail.com>
2019-12-11 19:21:01 -0500

    Restarted code & redefined what I want to do

---
+++ Hangman/ConsoleApplication1/Game/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanDriver
+{
+    class Driver
+    {
+        public void Main()
+        {
+            Console.Out("Welcome to Yoda's Gallows...Play at your own risk...");
+        }
+    }
+}
---
+++ Hangman/ConsoleApplication1/Game/Hangman.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanDriver
+{
+    class Hangman
+    {
+        private string[] words = {"lemon","peach","cherry" }; //to change into import from txt file of ridiculous words
+        private string secretWord;
+        private string hiddenWord;
+        private List<string> letterBank;
+        private string man;
+
+        public Hangman(){
+            Random gen = new Random();
+            int wordChoice = gen.Next(0, 3);
+
+            this.secretWord = words[wordChoice];
+
+            for(int i; i<this.secretWord.Length; i++){
+                this.hiddenWord += "-";
+            }
+
+            for(int j=1; j<=26; j++)
+            {
+                char alpha = Convert.ToChar(j + 94);
+                letterBank.Add(Convert.ToString(alpha));
+            }
+
+            string man = "";
+        }
+
+        public bool IsLetter(string guess){
+
+            return false; //test
+        }
+
+        public bool IsWord(string guess) {
+
+            return false; //test
+        }
+
+        public string toString(){
+            return "bruh" //test
+        }
+    }
+}
